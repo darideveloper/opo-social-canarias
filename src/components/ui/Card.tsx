d762f456b544d4da4e4f2d77e441b8006a2ce6fb
daridev <darideveloper@gmail.com>
2025-09-28 17:54:32 -0600

    style (Card): set min-w

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -13,7 +13,7 @@ const Card = forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm min-w-lg",
       className
     )}
     {...props}
