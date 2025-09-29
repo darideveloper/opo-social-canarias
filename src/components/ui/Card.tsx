725f8f00e65654940433a3c3135fa52b12226ae3
daridev <darideveloper@gmail.com>
2025-09-28 18:41:13 -0600

    style (Card): responsive with

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -26,7 +26,7 @@ const Card = forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm min-w-lg",
+      "rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-lg",
       className
     )}
     {...props}
