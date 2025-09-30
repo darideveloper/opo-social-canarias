da63ffd11e17358bf478ae8bcd7d7da4a993acc7
daridev <darideveloper@gmail.com>
2025-09-30 17:59:15 -0600

    fix (astro.config.mjs): read env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
+
+// Load from .env files (development) OR use process.env (production)
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
+const PUBLIC_API_BASE_URL = env.PUBLIC_API_BASE_URL || process.env.PUBLIC_API_BASE_URL;
+
+console.log(PUBLIC_API_BASE_URL)
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +22,7 @@ export default defineConfig({
     server: {
       proxy: {
         '/api': {
-          target: process.env.PUBLIC_API_BASE_URL || 'http://localhost:8000',
+          target: PUBLIC_API_BASE_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
           configure: (proxy, options) => {
