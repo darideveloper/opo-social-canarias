b26866800ce60ef0fbcbf9250ef201f1804fdeb0
daridev <darideveloper@gmail.com>
2025-10-06 12:37:38 -0600

    feat (settings): react setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
+  output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
+  integrations: [react()],
 });
