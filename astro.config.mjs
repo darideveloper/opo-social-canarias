c3f82213ffdddf40ab554a347fe5e1f29a55a3d2
daridev <darideveloper@gmail.com>
2025-10-06 13:04:06 -0600

    feat (settings): setup astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,20 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://socialia.com',
+
   output: 'server',
 
   adapter: node({
     mode: 'standalone',
   }),
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
