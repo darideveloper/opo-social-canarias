fad2cd784e0ef8734f5ee1e8c8c36d1483cd784a
daridev <darideveloper@gmail.com>
2025-10-11 17:01:01 -0600

    refactor (Image): separed classes in clsx

--- src/components/ui/Image.tsx
+++ src/components/ui/Image.tsx
@@ -1,17 +1,17 @@
-import clsx from "clsx";
+import clsx from 'clsx'
 
 type ImageProps = {
-  src: string;
-  alt: string;
-  className?: string;
-};
+  src: string
+  alt: string
+  className?: string
+}
 
 export default function Image({ src, alt, className }: ImageProps) {
   return (
     <img
       src={src}
       alt={alt}
-      className={clsx("object-cover w-142 rounded-2xl", className)}
+      className={clsx('object-cover', 'rounded-2xl', 'shadow-xl', className)}
     />
-  );
+  )
 }
