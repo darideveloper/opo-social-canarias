78df6e11294e7c0dce3fc20b74a9bd72f2158523
daridev <darideveloper@gmail.com>
2025-10-13 11:08:14 -0600

    fix (CardCentered): wrap original Card

---
+++ src/types/card.ts
@@ -0,0 +1,17 @@
+export type CardProps = {
+  title: string
+  iconName?:
+    | 'book'
+    | 'shield'
+    | 'bolt'
+    | 'bullseye'
+    | 'star'
+    | 'rocket'
+    | 'chart'
+    | 'lightbulb'
+    | 'trophy'
+    | 'graduation'
+  iconWhite?: boolean
+  textWhite?: boolean
+  class?: string
+}
