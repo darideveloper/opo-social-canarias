b5f320d7430d9d27c9247538221fc6a2cae2f791
daridev <darideveloper@gmail.com>
2025-10-21 15:01:10 -0600

    fix (HeroHome): responsive and change folder

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,8 @@ const config = {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './src/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+    './src/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './src/**/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
   theme: {
     extend: {
